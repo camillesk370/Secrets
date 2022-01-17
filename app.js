b5401532091a2497ba6a5af35108dc3ace701ccc
Camille <fenghua370@gmail.com>
2022-01-17 21:12:52 +0800

    Salt and hash passwords with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = request("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -37,43 +38,41 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User ({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser = new User ({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
   });
 
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
-      console.log(err);
-    }
-  });
 });
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+
   User.findOne({email: username}, function(err, foundUser) {
     if (!err) {
       if (foundUser) {
-        if (password === foundUser.password) {
-          res.render("secrets");
-        } else {
-          res.render("login", {checkPassword: "Email or password incorrect", username: username, password: password});
-        }
-      } else {
-        const newUser = new User ({
-          email: req.body.username,
-          password: req.body.password
-        });
-        newUser.save(function(err) {
-          if (!err) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
             res.render("secrets");
           } else {
-            console.log(err);
+            res.render("login", {checkPassword: "Email or password incorrect", username: username, password: password});
           }
         });
+      } else {
+        res.render("login", {checkPassword: "You haven't registered yet.", username: username, password: password});
       }
     } else {
       console.log(err);
